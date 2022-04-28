bea22029d5c381f5c8f2f2febd1af8f00d13b8ec
Park Min Hyeok <pmh_only@pmh.codes>
2022-04-28 11:43:29 +0900

    feat: add setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+yay -S xorg-server xorg-xinit xorg-xinput neovim conky dunst batsignal ibus ibus-hangul clight clight-gui discord firefox kitty picom yarn ruby-fusuma lxappearance xob nitrogen pipewire pipewire-pulse pipewire-alsa noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-d2coding bluez blueberry i3-gaps breeze-gtk breeze-icons breeze-obsidian-cursor-theme zsh i3-volume i3lock-color betterlockscreen wireplumber pnpm dmenu
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cp -r . ~/.
+startx
